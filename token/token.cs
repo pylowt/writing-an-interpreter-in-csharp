9e60207d56f916f76c45ff0040b1d98d3943d08d
Paul <paul.lowther01@hotmail.com>
2024-03-25 21:52:56 +0000

    Add initial token struct and constants

---
+++ token/token.cs
@@ -0,0 +1,38 @@
+public struct TokenType
+{
+    public string Value { get; set; }
+}
+
+public struct Token 
+{
+	public TokenType Type { get; set; }
+	public string Literal { get; set; }
+}
+
+
+static class Constants
+{
+	public const string ILLEGAL	 = "ILLEGAL";
+	public const string EOF		 = "EOF";
+
+	// Identifiers + literals
+	public const string IDENT	 = "IDENT";
+	public const string INT		 = "INT"; 
+
+	// Operators
+	public const string ASSIGN   = "=";
+	public const string PLUS	 = "+";
+
+	// Delimiters
+	public const string COMMA		 = ",";
+	public const string SEMICOLON	 = ";";
+
+	public const string	LPAREN	= "(";
+	public const string	RPAREN	= ")";
+	public const string	LBRACE	= "{";
+	public const string	RBRACE	= "}";
+
+	// Keywords
+	public const string FUNCTION = "FUNCTION";
+	public const string LET		 = "LET";
+}
