88abda299f1dfcb8c487c032e450a16e3753308c
Paul <paul.lowther01@hotmail.com>
2024-03-26 21:14:06 +0000

    Add namespace

--- InterpreterCs.Tests/Lexer_nextToken.cs
+++ InterpreterCs.Tests/Lexer_nextToken.cs
@@ -1,14 +1,17 @@
 namespace InterpreterCs.Tests;
+using token;
 
 public class Lexer_nextToken
 {
     [Fact]
     public void IsExpectedType()
     {
+		Dictionary<token.TokenType, string> tests = new Dictionary<string, string>();
+
 		string input = "=+(){},;";
-		var lexer = new Lexer(input);
+		//var lexer = new Lexer(input);
 		var expectedType = token.TokenType();
-        var result = lexer.nextToken();
+        //var result = lexer.nextToken();
 
     }
 }
