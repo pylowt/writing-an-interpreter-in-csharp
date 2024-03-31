a69f03c05e43a345e5623695abe93d8b6a7de332
Paul <paul.lowther01@hotmail.com>
2024-03-31 17:06:37 +0100

    Add changes to tokens and initial tests

--- InterpreterCs.Tests/Lexer_nextToken.cs
+++ InterpreterCs.Tests/Lexer_nextToken.cs
@@ -1,4 +1,5 @@
 namespace InterpreterCs.Tests;
+using System.Collections.Generic; 
 using token;
 
 public class Lexer_nextToken
@@ -6,12 +7,29 @@ public class Lexer_nextToken
     [Fact]
     public void IsExpectedType()
     {
-		Dictionary<token.TokenType, string> tests = new Dictionary<string, string>();
+		Dictionary<token.TokenType, string> tests = new Dictionary<token.TokenType, string>()
+		{
+			{ token.TokenType.ASSIGN, "="},
+			{ token.TokenTypes.PLUS, "+"},
+			{ token.TokenTypes.LPAREN, "("},
+			{ token.TokenTypes.RPAREN, ")"},
+			{ token.TokenTypes.LBRACE, "{"},
+			{ token.TokenTypes.RBRACE, "}"},
+			{ token.TokenTypes.COMMA, ","},
+			{ token.TokenTypes.SEMICOLON, ";"},
+			{ token.TokenTypes.EOF, ""},
+		};
 
 		string input = "=+(){},;";
-		//var lexer = new Lexer(input);
-		var expectedType = token.TokenType();
-        //var result = lexer.nextToken();
-
+		var lexer = new Lexer(input);
+		
+		foreach(KeyValuePair<token.TokenType, string> entry in tests)
+		{
+			var result = lexer.nextToken();
+			var typeMessage = $"{result.Type} - TokenType wrong. Expected {entry.key}";
+			Assert.True(entry.Key.Equals(result.Type), typeMessage);
+			var literalMessage = $"{result.Literal} - Literal wrong. Expected {entry.Value}";
+			Assert.True(entry.Value.Equals(result.Literal), literalMessage);
+		}
     }
 }
--- token/token.cs
+++ token/token.cs
@@ -2,17 +2,27 @@ namespace token;
 
 public struct TokenType
 {
-    public string Value { get; set; }
+	public readonly string Value;
+
+	public TokenType(string value)
+	{
+		Value = value;
+	}
 }
 
-public struct Token 
+public struct Token
 {
-	public TokenType Type { get; set; }
-	public string Literal { get; set; }
+	public readonly TokenType Type;
+	public readonly string Literal;
+
+	public Token(TokenType type, string literal)
+	{
+		Type = type;
+		Literal = literal;
+	}
 }
 
-
-static class Constants
+public static class TokenTypes
 {
 	public const string ILLEGAL	 = "ILLEGAL";
 	public const string EOF		 = "EOF";
@@ -22,13 +32,12 @@ static class Constants
 	public const string INT		 = "INT"; 
 
 	// Operators
-	public const string ASSIGN   = "=";
+	public const string ASSIGN = "=";
 	public const string PLUS	 = "+";
 
 	// Delimiters
 	public const string COMMA		 = ",";
 	public const string SEMICOLON	 = ";";
-
 	public const string	LPAREN	= "(";
 	public const string	RPAREN	= ")";
 	public const string	LBRACE	= "{";
@@ -38,4 +47,5 @@ static class Constants
 	public const string FUNCTION = "FUNCTION";
 	public const string LET		 = "LET";
 }
+
 
